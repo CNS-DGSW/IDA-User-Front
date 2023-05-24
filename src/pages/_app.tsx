38c5f3b156bc20c18d91d767ec398a6f4a27eb59
정우재 <dnwo0526@dgsw.hs.kr>
2023-05-24 22:50:22 +0900

    chore: 디자인 시스템 관련 라이브러리 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css"
 import type { AppProps } from "next/app"
 
+import { CNSThemeProvider } from "@dgswcns/react-styled-theme"
+import { useCNSThemeing } from "@dgswcns/react-theming"
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const theme = useCNSThemeing("LIGHT")
+  return (
+    <CNSThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </CNSThemeProvider>
+  )
 }
