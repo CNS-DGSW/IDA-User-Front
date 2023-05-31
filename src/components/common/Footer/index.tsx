c503ee4263e42c9a7f5cf28ccfb615ee4a6d9f2c
강성훈 <yolly050101@naver.com>
2023-05-31 16:44:03 +0900

    [fix] 스타일 변경

--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -1,17 +1,5 @@
 import React from "react"
-import {
-  DGSWLogo,
-  FooterLayout,
-  FooterInnerBox,
-  FooterParagraph,
-  CNSLogo,
-  FooterParagraphInnerBox,
-  FooterParagraphBox,
-  FooterLinkRow,
-  FooterLinkBox,
-  LinkImg,
-  TelLink,
-} from "./style"
+import * as S from "./style"
 import DGSWLogoPNG from "@/assets/DGSWLogo.png"
 import CNSLogoPNG from "@/assets/CNSLogo.png"
 import YoutubeSVG from "@/assets/youtube.svg"
@@ -19,51 +7,53 @@ import InstaSVG from "@/assets/instagram.svg"
 
 const Footer = () => {
   return (
-    <FooterLayout>
-      <FooterInnerBox>
+    <S.FooterLayout>
+      <S.FooterInnerBox>
         <a
           href="https://dgsw.dge.hs.kr/dgswh/main.do?sysId=dgswh"
           target="_blank"
           style={{ width: "104px" }}
           rel="noreferrer"
         >
-          <DGSWLogo src={DGSWLogoPNG} alt="dgsw" />
+          <S.DGSWLogo src={DGSWLogoPNG} alt="dgsw" />
         </a>
-        <FooterParagraph>
+        <S.FooterParagraph>
           원서접수처 주소 : 대구광역시 달성군 구지면 창리로 11길 93
           대구소프트웨어마이스터고등학교 ( 43010 )
-        </FooterParagraph>
-        <FooterParagraphBox>
-          <FooterParagraphInnerBox>
-            교무실 : <TelLink href="tel:053-231-9226">053-231-9226</TelLink>
-          </FooterParagraphInnerBox>
-          <FooterParagraphInnerBox>
-            행정실 : <TelLink href="tel:053-231-9215">053-231-9215</TelLink>
-          </FooterParagraphInnerBox>
-          <FooterParagraphInnerBox>FAX : 053-614-0709</FooterParagraphInnerBox>
-        </FooterParagraphBox>
-        <FooterParagraph>
+        </S.FooterParagraph>
+        <S.FooterParagraphBox>
+          <S.FooterParagraphInnerBox>
+            교무실 : <S.TelLink href="tel:053-231-9226">053-231-9226</S.TelLink>
+          </S.FooterParagraphInnerBox>
+          <S.FooterParagraphInnerBox>
+            행정실 : <S.TelLink href="tel:053-231-9215">053-231-9215</S.TelLink>
+          </S.FooterParagraphInnerBox>
+          <S.FooterParagraphInnerBox>
+            FAX : 053-614-0709
+          </S.FooterParagraphInnerBox>
+        </S.FooterParagraphBox>
+        <S.FooterParagraph>
           대구소프트웨어마이스터고등학교 CNS동아리
-          <CNSLogo src={CNSLogoPNG} alt="dgsw" />
-        </FooterParagraph>
-        <FooterLinkRow>
-          <FooterLinkBox
+          <S.CNSLogo src={CNSLogoPNG} alt="dgsw" />
+        </S.FooterParagraph>
+        <S.FooterLinkRow>
+          <S.FooterLinkBox
             href="https://www.youtube.com/@softwaremeister"
             target="_blank"
             rel="noreferrer"
           >
-            <LinkImg src={YoutubeSVG} alt="youtube" />
-          </FooterLinkBox>
-          <FooterLinkBox
+            <S.LinkImg src={YoutubeSVG} alt="youtube" />
+          </S.FooterLinkBox>
+          <s.FooterLinkBox
             href="https://www.instagram.com/dgsw.it/"
             target="_blank"
             rel="noreferrer"
           >
-            <LinkImg src={InstaSVG} alt="instagram" />
-          </FooterLinkBox>
-        </FooterLinkRow>
-      </FooterInnerBox>
-    </FooterLayout>
+            <S.LinkImg src={InstaSVG} alt="instagram" />
+          </s.FooterLinkBox>
+        </S.FooterLinkRow>
+      </S.FooterInnerBox>
+    </S.FooterLayout>
   )
 }
 
