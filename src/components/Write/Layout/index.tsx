e3a26ea3e92843c48c299f559c274bbfeb9fe0a3
강성훈 <yolly050101@naver.com>
2023-06-15 10:43:14 +0900

    [CHORE] 레이아웃 타이틀 타입 union 변경

--- src/components/Write/Layout/index.tsx
+++ src/components/Write/Layout/index.tsx
@@ -1,10 +1,7 @@
 import PageProvider from "@/components/common/Write/PageProvider"
-import React, { PropsWithChildren } from "react"
+import React from "react"
 import { LayoutContainer, LayoutTitle, WriteContainer } from "./style"
-
-interface WriteLayoutProps extends PropsWithChildren {
-  title: string
-}
+import { WriteLayoutProps } from "./types"
 
 /**
  *
