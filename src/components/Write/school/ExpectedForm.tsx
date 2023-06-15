0e62b0b058510892bf487d7b0615beac5d7dc1cf
강성훈 <yolly050101@naver.com>
2023-06-16 08:36:28 +0900

    [CHORE] 수정사항 반영

--- src/components/Write/school/ExpectedForm.tsx
+++ src/components/Write/school/ExpectedForm.tsx
@@ -2,23 +2,23 @@ import Input from "@/components/common/Input"
 import InputWrapper from "@/components/common/InputWrapper"
 import Select from "@/components/common/Select"
 import React from "react"
-import { SchoolButton, Wrap } from "./style"
+import * as S from "./style"
 
 const ExpectedForm = () => {
   return (
     <>
-      <Wrap justify="space-between">
+      <S.Wrap justify="space-between">
         <InputWrapper title="출신 중학교명">
-          <Wrap>
+          <S.Wrap>
             <Input type="text" readonly width={224} color="gray" />
-            <SchoolButton>학교 검색</SchoolButton>
-          </Wrap>
+            <S.SchoolButton>학교 검색</S.SchoolButton>
+          </S.Wrap>
         </InputWrapper>
         <InputWrapper title="NEIS 학교 번호">
           <Input type="text" readonly color="gray" />
         </InputWrapper>
-      </Wrap>
-      <Wrap justify="space-between">
+      </S.Wrap>
+      <S.Wrap justify="space-between">
         <InputWrapper title="지역명 ( 시도 )">
           <Select list={[1, 2, 3, 4]} />
         </InputWrapper>
@@ -28,15 +28,15 @@ const ExpectedForm = () => {
         <InputWrapper title="학교 연락처">
           <Input type="text" width={208} />
         </InputWrapper>
-      </Wrap>
-      <Wrap justify="space-between">
+      </S.Wrap>
+      <S.Wrap justify="space-between">
         <InputWrapper title="담임 성명">
           <Input type="text" />
         </InputWrapper>
         <InputWrapper title="담임 연락처">
           <Input type="text" />
         </InputWrapper>
-      </Wrap>
+      </S.Wrap>
     </>
   )
 }
