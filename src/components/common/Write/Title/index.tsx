837dddf94ff81821fab46c1e2d0da7eaca81e351
강성훈 <yolly050101@naver.com>
2023-06-15 11:27:31 +0900

    [CHORE] console 삭제

--- src/components/common/Write/Title/index.tsx
+++ src/components/common/Write/Title/index.tsx
@@ -1,14 +1,10 @@
-import React, { useEffect } from "react"
+import React from "react"
 import { usePageContext } from "../PageProvider"
 import { LayoutTitleStyle } from "../Layout/style"
 import { titleArray } from "@/constans/Write/titleConstant"
 
 export const LayoutTitle = () => {
   const { currentPage } = usePageContext()
-
-  useEffect(() => {
-    console.log(titleArray[currentPage - 1])
-  }, [currentPage])
 
   return <LayoutTitleStyle>{titleArray[currentPage - 1]}</LayoutTitleStyle>
 }
