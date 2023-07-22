2ea5411760c200c9077601884c50ada8f34fa1bd
bae0203 <gmbae06@dgsw.hs.kr>
2023-07-22 21:31:06 +0900

    feat : Add Inform Detail Page

---
+++ src/components/inform/detail/index.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react"
+import * as S from "./style"
+import type { IInformContentsProps } from "../type"
+import Footer from "@/components/common/Footer"
+
+const InformDetailPage = () => {
+  const [informData] = useState<IInformContentsProps>({
+    title: "입학전형 관련 공지",
+    date: "2023/10/01",
+    isImportant: true,
+  })
+  const informContext = `비포스쿨 공지입니다~ 샬라샬라 샬라샬라 이렇게 하시구 저희 학교는 대구 구지에 위치하고 있습니다~ 샬라샬라 비포스쿨 공지입니다~ 샬라샬라 샬라샬라 이렇게 하시구 저희 학교는 대구 구지에 위치하고 있습니다~ 샬라샬라 비포스쿨 공지입니다~ 샬라샬라 샬라샬라 이렇게 하시구 저희 학교는 대구 구지에 위치하고 있습니다~ 샬라샬라 비포스쿨 공지입니다~ 샬라샬라 샬라샬라 이렇게 하시구 저희 학교는 대구 구지에 위치하고 있습니다~ 샬라샬라 비포스쿨 공지입니다~ 샬라샬라 샬라샬라 이렇게 하시구 저희 학교는 대구 구지에 위치하고 있습니다~ 샬라샬라 비포스쿨 공지입니다~ 샬라샬라 샬라샬라 이렇게 하시구 저희 학교는 대구 구지에 위치하고 있습니다~ 샬라샬라 
+
+[준비물] 
+스케치북 
+유성매직 
+곰인형 
+꽃무늬
+이불`
+  return (
+    <S.MainComponentLayout>
+      <S.InformDetailContentsCol>
+        <S.InformDetailNotificationIconBox />
+        <S.InformDetailTitleRow>
+          <S.InformDetailSubTitleRow>
+            <S.InformDetailTitleParagraph>
+              {informData.title}
+            </S.InformDetailTitleParagraph>
+            <div>
+              <S.ImportantCheckBox isImportant={informData.isImportant}>
+                {informData.isImportant ? "주요공지" : "일반공지"}
+              </S.ImportantCheckBox>
+            </div>
+          </S.InformDetailSubTitleRow>
+          <S.InformDetailSubTitleRow>
+            <S.InformDetailDateIconBox />
+            <S.InformDetailDateContextParagraph>
+              {informData.date}
+            </S.InformDetailDateContextParagraph>
+          </S.InformDetailSubTitleRow>
+        </S.InformDetailTitleRow>
+        <S.InformDetailContextParagraph>
+          {informContext}
+        </S.InformDetailContextParagraph>
+        <S.InformDetailBackHyperlinkBox>
+          {"< 공지사항으로 돌아가기"}
+        </S.InformDetailBackHyperlinkBox>
+      </S.InformDetailContentsCol>
+      <Footer />
+    </S.MainComponentLayout>
+  )
+}
+
+export default InformDetailPage
