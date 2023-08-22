1843172cf0b2309dc06313f7accce4e20933767a
정우재 <80890472+woojae05@users.noreply.github.com>
2023-08-21 22:13:33 -0700

    [FEAT] WriteIdPhoto component

--- src/pages/apply.tsx
+++ src/pages/apply.tsx
@@ -3,12 +3,13 @@ import WriteDocument from "@/components/Write/document"
 import WriteSchool from "@/components/Write/school"
 import WriteType from "@/components/Write/type"
 import WritePersonal from "@/components/Write/personal"
+import WriteIdPhoto from "@/components/Write/idPhoto"
 const apply = () => {
   return (
     <WriteLayout>
       <WritePersonal />
       <div>2</div>
-      <div>3</div>
+      <WriteIdPhoto />
       <WriteSchool />
       <WriteType />
       <div>6</div>
