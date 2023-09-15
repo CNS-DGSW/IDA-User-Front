300a050bac7c7901ce4971a68db702e66bbbf6f5
sueeeeh <suehyun51@gmail.com>
2023-09-15 09:24:56 +0900

    feat/#7

---
+++ src/components/Write/guardian/index.tsx
@@ -0,0 +1,44 @@
+import Card from "@/components/common/Card"
+import InputWrapper from "@/components/common/InputWrapper"
+import Input from "@/components/common/Input"
+import React, { ChangeEvent, useState } from "react"
+import Select from "@/components/common/Select"
+import { GuardianRelation } from "@/constants/Write/guardianRelationConstant"
+import CustomDatePicker from "@/components/common/DatePicker"
+import * as S from "./style"
+
+const WriteGuardian = () => {
+  return (
+    <section>
+      <Card>
+        <InputWrapper>
+          <InputWrapper title="성명">
+            <Input type="text" width={310} />
+          </InputWrapper>
+          <InputWrapper title="지원자와의 관계">
+            <Select list={GuardianRelation} width={310} />
+          </InputWrapper>
+        </InputWrapper>
+        <InputWrapper style={{ marginTop: 54 }}>
+          <InputWrapper title="생년월일" style={{ position: "relative" }}>
+            <Input type="text" disabled />
+            <S.CalanderImgBox>
+              <CustomDatePicker />
+            </S.CalanderImgBox>
+          </InputWrapper>
+          <InputWrapper title="휴대폰">
+            <Input type="tel" width={310} />
+          </InputWrapper>
+        </InputWrapper>
+        <InputWrapper title="주소" style={{ marginTop: 54 }}>
+          <Input type="text" width={650} />
+        </InputWrapper>
+        <InputWrapper title="상세주소" style={{ marginTop: 34 }}>
+          <Input type="text" width={650} />
+        </InputWrapper>
+      </Card>
+    </section>
+  )
+}
+
+export default WriteGuardian
---
+++ src/constants/Write/guardianRelationConstant.ts
@@ -0,0 +1,7 @@
+export type GuardianRelationType = "부" | "모" | "추가 필요"
+
+export const GuardianRelation: GuardianRelationType[] = [
+  "부",
+  "모",
+  "추가 필요",
+]
