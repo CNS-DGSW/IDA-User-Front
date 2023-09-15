92b3645790a11611149e0fce373918509e5c945e
sueeeeh <suehyun51@gmail.com>
2023-09-15 09:37:06 +0900

    feature/#7

--- src/pages/apply.tsx
+++ src/pages/apply.tsx
@@ -3,11 +3,12 @@ import WriteDocument from "@/components/Write/document"
 import WriteSchool from "@/components/Write/school"
 import WriteType from "@/components/Write/type"
 import WritePersonal from "@/components/Write/personal"
+import WriteParent from "@/components/Write/guardian"
 const apply = () => {
   return (
     <WriteLayout>
       <WritePersonal />
-      <div>2</div>
+      <WriteParent />
       <div>3</div>
       <WriteSchool />
       <WriteType />
