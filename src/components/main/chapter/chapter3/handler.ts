c1cbe5dc5bc93666ba22a986da104bf8cb63dc3f
bae0203 <gmbae06@dgsw.hs.kr>
2023-09-16 12:57:23 +0900

    feat : Handler Button Information

---
+++ src/components/main/chapter/chapter3/handler.ts
@@ -0,0 +1,29 @@
+import HomeIcon from "../../../../../public/asset/Icon/Home.svg"
+import Notification from "../../../../../public/asset/Icon/Notification.svg"
+import MessageIcon from "../../../../../public/asset/Icon/Home.svg"
+
+interface IhandlerInfo {
+  image: string
+  context: string
+  backgroundColor: string
+}
+
+const handlerInfo: IhandlerInfo[] = [
+  {
+    image: HomeIcon,
+    context: "DGSW 홈페이지 바로가기",
+    backgroundColor: "#1485EE",
+  },
+  {
+    image: Notification,
+    context: "1차 합격 결과 확인하기",
+    backgroundColor: "#8EC9FF",
+  },
+  {
+    image: MessageIcon,
+    context: "최종 합격 결과 확인하기",
+    backgroundColor: "#1E2F44",
+  },
+]
+
+export default handlerInfo
