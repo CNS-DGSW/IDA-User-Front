6afbff688ad7c89e2f23ceef0938916a202bb73f
bae0203 <gmbae06@dgsw.hs.kr>
2023-09-16 12:58:33 +0900

    linting design : Add Handler Design

---
+++ src/components/main/chapter/chapter3/style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+export const ContentsLayoutCol = styled.div`
+    width : 1000px
+    margin: 0 auto;
+    min-height : 500px;
+`
+
+export const Chapter3Layout = styled.div`
+  min-height: 300vh;
+`
+export const HandlerWrapRow = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`
