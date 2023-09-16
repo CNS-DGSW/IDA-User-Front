9457392a0001f64a472c4161192f0d2e25fc684e
bae0203 <gmbae06@dgsw.hs.kr>
2023-09-16 18:15:23 +0900

    feat : view Contents Component

---
+++ src/components/common/DateChecker/viewAll/contents/style.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components"
+
+export const IconImageBox = styled.div`
+  width: 120px;
+  height: 120px;
+  border-radius: 24px;
+  background-color: #f8fafd;
+`
+
+export const TitleParagragh = styled.p`
+  font-size: 1.5rem;
+  font-weight: 600;
+  color: #1e2f44;
+  margin: 1.5rem 0 0.5rem 0;
+`
+
+export const ContextParagragh = styled.p`
+  font-size: 1rem;
+  font-weight: 500;
+  color: #808488;
+  line-height: 24px;
+  letter-spacing: -0.7px;
+`
+
+export const ContentsLayout = styled.div`
+  width: 400px;
+`
