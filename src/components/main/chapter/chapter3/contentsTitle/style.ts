c876e2b6b88913ee1f3451a3d7d0dbd017f0fd3f
bae0203 <gmbae06@dgsw.hs.kr>
2023-09-16 16:53:19 +0900

    update : Context Color

--- src/components/main/chapter/chapter3/contentsTitle/style.ts
+++ src/components/main/chapter/chapter3/contentsTitle/style.ts
@@ -6,12 +6,14 @@ export const TitleParagragh = styled.p`
   line-height: 40px;
   width: 600px;
   word-break: keep-all;
+  color: #1e2f44;
 `
 export const ContextParagragh = styled.p`
   font-size: 1rem;
   line-height: 30px;
   font-weight: medium;
-  margin-top: 1.75rem;
+  margin: 1rem 0;
   width: 100%;
   word-break: keep-all;
+  color: #424344;
 `
