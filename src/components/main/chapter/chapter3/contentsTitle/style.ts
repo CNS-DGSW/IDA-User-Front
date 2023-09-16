1ce2b00af3c6b077e8bc4240ac7b80480cd66e2c
bae0203 <gmbae06@dgsw.hs.kr>
2023-09-16 14:20:01 +0900

    feat : Contents Title Components

---
+++ src/components/main/chapter/chapter3/contentsTitle/style.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+export const TitleParagragh = styled.p`
+  font-size: 2rem;
+  font-weight: bold;
+  line-height: 40px;
+  width: 600px;
+  word-break: keep-all;
+`
+export const ContextParagragh = styled.p`
+  font-size: 1rem;
+  line-height: 30px;
+  font-weight: medium;
+  margin-top: 1.75rem;
+  width: 100%;
+  word-break: keep-all;
+`
