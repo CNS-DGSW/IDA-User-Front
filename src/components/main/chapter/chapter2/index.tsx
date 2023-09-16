c9a6163570010ea133a54b60543e6b8717f71e11
bae0203 <gmbae06@dgsw.hs.kr>
2023-09-16 18:16:15 +0900

    fix : eslint

--- src/components/main/chapter/chapter2/index.tsx
+++ src/components/main/chapter/chapter2/index.tsx
@@ -1,12 +1,15 @@
-import { useSetRecoilState } from "recoil"
 import { schedules } from "@/constants/schedule"
 import useIntersectionObserver from "@/hooks/common/useIntersectionObserver"
 import { isDarkNavbarState } from "@/atom/navBarAtom"
 import DateChecker from "../../../common/DateChecker"
 import ScheduleNavigator from "@/components/ScheduleNavigator"
+import { useRecoilValue, useSetRecoilState } from "recoil"
+import { currentScheduleState } from "@/atom/scheduleAtom"
 import * as S from "./style"
+import ViewAll from "@/components/common/DateChecker/viewAll"
 
 const Chapter2 = () => {
+  const currentScheuldeNavState = useRecoilValue(currentScheduleState)
   const setDarkNavbarState = useSetRecoilState(isDarkNavbarState)
   const onIntersect: IntersectionObserverCallback = ([{ isIntersecting }]) => {
     setDarkNavbarState(!isIntersecting)
@@ -17,9 +20,10 @@ const Chapter2 = () => {
     <S.Chapter2Layout ref={setTarget}>
       <S.ScheduleSectionRow>
         <S.ScheduleSection>
-          {schedules.map((schedule) => {
-            return <DateChecker key={schedule.title} schedule={schedule} />
-          })}
+          {schedules.map((schedule) => (
+            <DateChecker key={schedule.title} schedule={schedule} />
+          ))}
+          {currentScheuldeNavState === "전체 보기" && <ViewAll />}
         </S.ScheduleSection>
         <ScheduleNavigator />
       </S.ScheduleSectionRow>
