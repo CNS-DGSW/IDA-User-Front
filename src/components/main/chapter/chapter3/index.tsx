a83955fbfac26c782bf6470b4311f7287feb02af
bae0203 <gmbae06@dgsw.hs.kr>
2023-09-16 12:57:44 +0900

    feat : Handler Button

--- src/components/main/chapter/chapter3/index.tsx
+++ src/components/main/chapter/chapter3/index.tsx
@@ -1,14 +1,29 @@
 import React from "react"
+import HandlerBox from "./handlerbox"
+import * as S from "./style"
+import handlerInfo from "./handler"
 
 const Chapter3 = () => {
   return (
-    <>
+    <S.Chapter3Layout>
       <div>
-        <p>대소고로 온나온나 온나...</p>
-        <p>오라고,,,</p>
+        <div>
+          <p>대소고로 온나온나 온나...</p>
+          <p>오라고,,,</p>
+        </div>
+        <button></button>
       </div>
-      <div className="layout"></div>
-    </>
+      <S.ContentsLayoutCol className="layout">
+        <S.HandlerWrapRow>
+          {handlerInfo.map((e) => (
+            <HandlerBox
+              context={e.context}
+              backgroundColor={e.backgroundColor}
+            />
+          ))}
+        </S.HandlerWrapRow>
+      </S.ContentsLayoutCol>
+    </S.Chapter3Layout>
   )
 }
 
