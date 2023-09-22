dbdc5807b11bb1a4740bf5ea01ac98e39e2a03f9
bae0203 <gmbae06@dgsw.hs.kr>
2023-09-22 14:26:55 +0900

    feat : add Onchange Value

--- src/components/Write/subject/subjectTable/index.tsx
+++ src/components/Write/subject/subjectTable/index.tsx
@@ -1,11 +1,18 @@
 import Button from "@/components/common/Button"
-import Input from "@/components/common/Input"
 import Select from "@/components/common/Select"
 import { Table } from "@/components/common/table"
 import { grade, schoolYear, subjectNames } from "@/constants/Write/subject"
-import React, { useState } from "react"
+import React, { useLayoutEffect, useState } from "react"
 import { ButtonWrapper } from "./style"
 import { useArray } from "../hooks/useArray"
+import {
+  type IInitValue,
+  useSetInitValue,
+  useOnChangeValue,
+  useAddValue,
+  useFreeSemester,
+} from "../hooks/useSubjectValue"
+import { UsePrompt } from "../prompt"
 
 interface IActiveSubjectHandler {
   year: string
@@ -13,8 +20,10 @@ interface IActiveSubjectHandler {
 }
 
 const SubjectTable = () => {
+  const [addSubjectValue, setAddSubjectValue] = useState<string>("")
+  const [activeAddSubject, setActiveAddSubject] = useState<boolean>(false)
   const [otherSubject, setOtherSubject] = useState<string[]>([])
-  const addOtherSubject = () => setOtherSubject((prev) => [...prev, ""])
+  const [subjectScoreValue, setSubjectScoreValue] = useState<IInitValue[]>([])
   const [activeSubject, setActiveSubject] = useState<boolean[]>([
     false,
     false,
@@ -23,91 +32,163 @@ const SubjectTable = () => {
     false,
     false,
   ])
-  const ChangeArray = useArray({
+
+  const activeArray = useArray({
     state: activeSubject,
     setState: setActiveSubject,
   })
 
-  // const ChangeActiveSubject = () => {}
+  useLayoutEffect(() => {
+    useSetInitValue({
+      setState: setSubjectScoreValue,
+    })
+  }, [])
+
+  const addOtherSubject = () => {
+    if (subjectNames.find((e) => e === addSubjectValue)) {
+      alert("과목이 이미 추가되있습니다.")
+    } else {
+      useAddValue({
+        subject: addSubjectValue,
+        state: subjectScoreValue,
+        setState: setSubjectScoreValue,
+      })
+      setOtherSubject((prev) => [...prev, addSubjectValue])
+      setAddSubjectValue("")
+      setActiveAddSubject(false)
+    }
+  }
+
+  const ChangeActiveSubject = (index: number) => {
+    useFreeSemester({
+      semesterIndex: index,
+      state: subjectScoreValue,
+      setState: setSubjectScoreValue,
+    })
+    activeArray.ChangeState({
+      changeIndex: index,
+      changeValue: !activeSubject[index],
+    })
+  }
 
   const ActiveSubjectHandler = ({ year, semester }: IActiveSubjectHandler) => {
+    console.log(subjectScoreValue)
+
     switch (year) {
       case "1학년":
-        if (semester === 1)
-          ChangeArray({ changeIndex: 0, changeValue: !activeSubject[0] })
-        else if (semester === 2)
-          ChangeArray({ changeIndex: 1, changeValue: !activeSubject[1] })
+        if (semester === 1) ChangeActiveSubject(0)
+        else if (semester === 2) ChangeActiveSubject(1)
         break
       case "2학년":
-        if (semester === 1)
-          ChangeArray({ changeIndex: 2, changeValue: !activeSubject[2] })
-        else if (semester === 2)
-          ChangeArray({ changeIndex: 3, changeValue: !activeSubject[3] })
+        if (semester === 1) ChangeActiveSubject(2)
+        else if (semester === 2) ChangeActiveSubject(3)
         break
       case "3학년":
-        if (semester === 1)
-          ChangeArray({ changeIndex: 4, changeValue: !activeSubject[4] })
-        else if (semester === 2)
-          ChangeArray({ changeIndex: 5, changeValue: !activeSubject[5] })
+        if (semester === 1) ChangeActiveSubject(4)
+        else if (semester === 2) ChangeActiveSubject(5)
+        break
+    }
+  }
+
+  const SetBackgroundColor = ({
+    year,
+    semester,
+  }: IActiveSubjectHandler): number | undefined => {
+    switch (year.substring(0, 1)) {
+      case "1":
+        if (semester === 1) return 0
+        if (semester === 2) return 1
+        break
+      case "2":
+        if (semester === 1) return 2
+        if (semester === 2) return 3
+        break
+      case "3":
+        if (semester === 1) return 4
+        if (semester === 2) return 5
         break
     }
   }
 
   return (
     <>
+      {activeAddSubject && (
+        <UsePrompt
+          addFunc={addOtherSubject}
+          value={addSubjectValue}
+          setValue={setAddSubjectValue}
+          setActive={setActiveAddSubject}
+        />
+      )}
       <Table customStyle={{ marginBottom: "18px" }}>
         <Table.Header>
           <Table.Tr height="162">
             <Table.Th width="74">과목</Table.Th>
-            {schoolYear.map((year) => (
-              <Table.Th width="168" notBorder key={year}>
-                <Table.Tr>
-                  <Table.Th width="168">{year}</Table.Th>
-                </Table.Tr>
-                <Table.Tr>
-                  <Table.Th width="84" background="F9F9F9" color="000">
-                    1학기
-                  </Table.Th>
-                  <Table.Th width="84" background="F9F9F9" color="000">
-                    2학기
-                  </Table.Th>
-                </Table.Tr>
-                <Table.Tr>
-                  <Table.Th width="84" background="fff">
-                    <Button
-                      color="primary"
-                      style={{
-                        width: "73px",
-                        height: "38px",
-                        margin: "0 auto",
-                      }}
-                      radius={6}
-                      onClick={() =>
-                        ActiveSubjectHandler({ year, semester: 1 })
-                      }
-                    >
-                      자유학기제
-                    </Button>
-                  </Table.Th>
-                  <Table.Th width="84" background="fff">
-                    <Button
-                      color="primary"
-                      style={{
-                        width: "73px",
-                        height: "38px",
-                        margin: "0 auto",
-                      }}
-                      onClick={() => {
-                        ActiveSubjectHandler({ year, semester: 2 })
-                      }}
-                      radius={6}
-                    >
-                      자유학기제
-                    </Button>
-                  </Table.Th>
-                </Table.Tr>
-              </Table.Th>
-            ))}
+            {schoolYear.map((year) => {
+              const activeIndex: number[] | null = [
+                SetBackgroundColor({
+                  year,
+                  semester: 1,
+                }) ?? 0,
+                SetBackgroundColor({
+                  year,
+                  semester: 2,
+                }) ?? 0,
+              ]
+              return (
+                <Table.Th width="168" notBorder key={year}>
+                  <Table.Tr>
+                    <Table.Th width="168">{year}</Table.Th>
+                  </Table.Tr>
+                  <Table.Tr>
+                    <Table.Th width="84" background="F9F9F9" color="000">
+                      1학기
+                    </Table.Th>
+                    <Table.Th width="84" background="F9F9F9" color="000">
+                      2학기
+                    </Table.Th>
+                  </Table.Tr>
+                  <Table.Tr>
+                    <Table.Th width="84" background="fff">
+                      <Button
+                        color={
+                          activeSubject[activeIndex[0]] ? "unable" : "primary"
+                        }
+                        style={{
+                          width: "73px",
+                          height: "38px",
+                          margin: "0 auto",
+                        }}
+                        radius={6}
+                        onClick={() => {
+                          ActiveSubjectHandler({ year, semester: 1 })
+                        }}
+                      >
+                        자유학기제
+                      </Button>
+                    </Table.Th>
+                    <Table.Th width="84" background="fff">
+                      <Button
+                        color={
+                          activeSubject[activeIndex[1]] ? "unable" : "primary"
+                        }
+                        style={{
+                          width: "73px",
+                          height: "38px",
+                          margin: "0 auto",
+                        }}
+                        onClick={() => {
+                          ActiveSubjectHandler({ year, semester: 2 })
+                        }}
+                        radius={6}
+                      >
+                        자유학기제
+                      </Button>
+                    </Table.Th>
+                  </Table.Tr>
+                </Table.Th>
+              )
+            })}
             <Table.Th width="70">삭제</Table.Th>
           </Table.Tr>
         </Table.Header>
@@ -122,12 +203,28 @@ const SubjectTable = () => {
                     <Select
                       width={73}
                       list={grade}
-                      changeEvent={() => {}}
-                      value={activeSubject[idx] ? "-" : "A"}
+                      changeEvent={(e: any) => {
+                        console.log(idx + 6 * subjectNameIndex)
+
+                        useOnChangeValue({
+                          changeValue: e.target.innerText,
+                          index: idx + 6 * subjectNameIndex,
+                          state: subjectScoreValue,
+                          setState: setSubjectScoreValue,
+                        })
+                      }}
+                      value={
+                        activeSubject[idx]
+                          ? "-"
+                          : subjectScoreValue[idx + 6 * subjectNameIndex]?.score
+                      }
                       style={{
                         padding: "0 8px",
                         margin: "0 auto",
                         fontSize: "12px",
+                        backgroundColor: `${
+                          activeSubject[idx] ? "#F2F3F5" : "white"
+                        }`,
                       }}
                       key={idx}
                       direction={
@@ -141,20 +238,9 @@ const SubjectTable = () => {
               <Table.Td width="70"></Table.Td>
             </Table.Tr>
           ))}
-          {otherSubject.map((_, index) => (
+          {otherSubject.map((newSubjectName, index) => (
             <Table.Tr height="60" key={index}>
-              <Table.Td width="74">
-                <Input
-                  type="text"
-                  width={56}
-                  style={{
-                    padding: "2px",
-                    height: "30px",
-                    margin: "0 auto",
-                    textAlign: "center",
-                  }}
-                />
-              </Table.Td>
+              <Table.Td width="74">{newSubjectName}</Table.Td>
               {Array(6)
                 .fill(0)
                 .map((_, idx) => (
@@ -162,12 +248,26 @@ const SubjectTable = () => {
                     <Select
                       width={73}
                       list={grade}
-                      changeEvent={() => {}}
-                      value={activeSubject[idx] ? "-" : "A"}
+                      changeEvent={(e: any) =>
+                        useOnChangeValue({
+                          changeValue: e.target.innerText,
+                          index: idx + 6 * index + 72,
+                          state: subjectScoreValue,
+                          setState: setSubjectScoreValue,
+                        })
+                      }
+                      value={
+                        activeSubject[idx]
+                          ? "-"
+                          : subjectScoreValue[idx + 6 * index + 72]?.score
+                      }
                       style={{
                         padding: "0 8px",
                         margin: "0 auto",
                         fontSize: "12px",
+                        backgroundColor: `${
+                          activeSubject[idx] ? "#F2F3F5" : "white"
+                        }`,
                       }}
                       key={idx}
                       direction={otherSubject.length - index > 2 ? true : false}
@@ -197,7 +297,7 @@ const SubjectTable = () => {
         <Button
           radius={8}
           style={{ width: "109px", height: "40px" }}
-          onClick={addOtherSubject}
+          onClick={() => setActiveAddSubject(true)}
         >
           과목추가
         </Button>
