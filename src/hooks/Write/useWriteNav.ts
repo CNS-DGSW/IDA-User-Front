6ce8a937b7549d3cc9a0e4c20d0afcab6de195a2
bae0203 <gmbae06@dgsw.hs.kr>
2024-01-16 23:52:54 +0900

    feat: resize window size

--- src/hooks/Write/useWriteNav.ts
+++ src/hooks/Write/useWriteNav.ts
@@ -1,8 +1,16 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const useWriteNav = () => {
   const isBrowser = typeof window !== "undefined"
-  const [width] = useState<number>(isBrowser ? window.innerWidth : 0)
+  const [width, setWidth] = useState<number>(isBrowser ? window.innerWidth : 0)
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth)
+    window.addEventListener("resize", handleResize)
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
+  }, [])
 
   const checkWidth = (isSameIdx: boolean): boolean => {
     if (width > 500) return true
