7f4eef04bc348403714fe131acc6c8320360ef4c
bae0203 <gmbae06@dgsw.hs.kr>
2024-01-18 22:51:08 +0900

    feat : 틀 제작

---
+++ src/components/Write/school/schoolSearchModal/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import * as S from "./style"
+
+const SchoolSearchModal = () => {
+  return (
+    <S.ModalLayout>
+      <S.SubLayout>
+        <p>SchoolSearchModal</p>
+      </S.SubLayout>
+    </S.ModalLayout>
+  )
+}
+
+export default SchoolSearchModal
