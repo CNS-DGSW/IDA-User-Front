f3935f8f90745f62d0cf8e6915ff013c03dc6eb9
bae0203 <gmbae06@dgsw.hs.kr>
2024-02-04 17:21:09 +0900

    fix : 코드 리뷰

--- src/components/Write/subject/hooks/useSubjectTable.ts
+++ src/components/Write/subject/hooks/useSubjectTable.ts
@@ -14,70 +14,74 @@ const useSubjectTable = () => {
       }
     else
       return {
-        width: "26px",
-        height: "20px",
+        width: "1.625rem",
+        height: "1.25rem",
         margin: "0 auto",
-        fontSize: "5px",
+        fontSize: "0.25rem",
       }
   }
 
   const setSelectStyle = () => {
     if (browserWidth > 500)
       return {
-        padding: "0 8px",
+        padding: "0 0.5rem",
         margin: "0 auto",
-        fontSize: "12px",
+        fontSize: "0.75rem",
       }
     else
       return {
         margin: "0 auto",
-        fontSize: "10px",
-        width: "26px",
-        maxWidth: "26px",
+        fontSize: "0.625rem",
+        width: "1.625rem",
+        maxWidth: "1.625rem",
         padding: "0 0.125rem",
-        height: "20px",
+        height: "1.25rem",
       }
   }
 
   const setNewSubjectBtnStyle = () => {
     if (browserWidth > 500) return { width: "109px", height: "40px" }
-    else return { width: "75px", height: "30px", fontSize: "10px" }
+    else return { width: "4.6875rem", height: "1.875rem", fontSize: "0.625rem" }
   }
 
   const setScoreChangeStyle = () => {
     if (browserWidth > 500)
-      return { fontSize: "14px", padding: "0 12px 0 16px", height: "40px" }
+      return {
+        fontSize: "0.875rem",
+        padding: "0 0.75rem 0 1rem",
+        height: "2.5rem",
+      }
     else
       return {
-        fontSize: "10px",
-        padding: "0 12px 0 16px",
-        height: "30px",
-        width: "120px",
+        fontSize: "0.625rem",
+        padding: "0 0.75rem 0 1rem",
+        height: "1.875rem",
+        width: "7.5rem",
       }
   }
 
   const setScoreListStyle = () => {
-    if (browserWidth > 500) return "14px"
-    else return "6px"
+    if (browserWidth > 500) return "0.875rem"
+    else return "0.375rem"
   }
 
   const setNewSubject = (): CSSProperties => {
     if (browserWidth > 500)
       return {
-        padding: "2px",
-        height: "30px",
-        width: "56px",
+        padding: "0.125rem",
+        height: "1.875rem",
+        width: "3.5rem",
         margin: "0 auto",
         textAlign: "center",
       }
     else
       return {
-        padding: "2px",
-        height: "10px",
+        padding: "0.125rem",
+        height: "0.625rem",
         margin: "0 auto",
         textAlign: "center",
         width: "15px",
-        fontSize: "4px",
+        fontSize: "0.25rem",
       }
   }
 
