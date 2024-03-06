ae217601f3aeaf16847609c7cecf9d8df78e0737
bae0203 <gmbae06@dgsw.hs.kr>
2024-03-06 20:17:29 +0900

    fix : get Value

--- src/components/Write/API.ts
+++ src/components/Write/API.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const token =
-  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJtZW1iZXJJZCI6MjQ2MTc2LCJ0eXBlIjoiYWNjZXNzIiwiaWF0IjoxNzA5NjEzODU4LCJleHAiOjE3MDk2MTU2NTh9.-E6QPo9SDKPdiAxH07zlqigxMLxshJrgUWrMKyfVdHQ"
+  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJtZW1iZXJJZCI6MjQ2MTc2LCJ0eXBlIjoiYWNjZXNzIiwiaWF0IjoxNzA5Njg2MzAwLCJleHAiOjE3MDk2ODgxMDB9.aVbWNmjM7OROK2jurRzFUptqZoye3MfY3UAgWIPGCng"
 
 const Api = axios.create({
   baseURL: "http://52.79.189.147:8080",
