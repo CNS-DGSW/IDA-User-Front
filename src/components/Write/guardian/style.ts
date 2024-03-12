2f40296f2827564fab957f9e00aeecf731419854
bae0203 <gmbae06@dgsw.hs.kr>
2024-03-12 15:06:37 +0900

    delete : 안쓰는 스타일 삭제

--- src/components/Write/guardian/style.ts
+++ src/components/Write/guardian/style.ts
@@ -2,19 +2,6 @@ import styled from "styled-components"
 
 export const GuardianSection = styled.section``
 
-export const DaumModalWrapper = styled.div`
-  position: fixed;
-  z-index: 10;
-  background-color: rgba(0, 0, 0, 0.1);
-  width: 100vw;
-  height: 100vh;
-  top: 0;
-  left: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`
-
 export const CalanderImgBox = styled.div`
   position: absolute;
   right: 18px;
