c17c7202fa00aedf7206eb046580b72a0a73f9ad
sueeeeh <suehyun51@gmail.com>
2024-04-22 20:32:24 +0900

    chore/로그인 후 로직 추가

--- src/hooks/SignIn/useSignInQuery.ts
+++ src/hooks/SignIn/useSignInQuery.ts
@@ -1,9 +1,11 @@
 import SignInrepositoryImpl from "@/apis/SignIn/SignIn.repositoryImpl"
 import type { SignInPropsType, SignInResType } from "@/types/SignIn/signIn.type"
+import { useRouter } from "next/router"
 import { useMutation } from "react-query"
 import type { UseMutationOptions } from "react-query"
 
 export const usePostSignInQuery = () => {
+    const router = useRouter()
     return useMutation(
         async(
             postPropsData: SignInPropsType,
@@ -13,9 +15,11 @@ export const usePostSignInQuery = () => {
             >
         ) => await SignInrepositoryImpl.postSignIn(postPropsData),
         {
-            onSuccess: (res) => {
+            onSuccess: async(res) => {
                 localStorage.setItem("accessToken",res.data.accessToken)
                 localStorage.setItem("refreshToken",res.data.refreshToken)
+                alert("로그인에 성공했습니다.")
+                await router.push("/")
             }
         }
     )
