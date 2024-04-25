9e637f72e6f4320b3011d2f3ac33574f50f90e0b
sueeeeh <suehyun51@gmail.com>
2024-04-25 20:33:26 +0900

    chore/type 수정

--- src/apis/SignUp/signUp.repositoryImpl.ts
+++ src/apis/SignUp/signUp.repositoryImpl.ts
@@ -1,7 +1,8 @@
+import type { SignUprepositoryType } from "@/types/Queries/SignUp/signUp.repositoryType"
 import type { SignUpPropsType } from "@/types/SignUp/signUp.type"
 import { IDAcustomAxios } from "@/util/CustomAxios/customAxios"
 
-class SignUprepositoryImpl implements SignUprepositoryImpl {
+class SignUprepositoryImpl implements SignUprepositoryType {
   public async postSignUp(postSignUpPropsData: SignUpPropsType): Promise<void> {
     return await IDAcustomAxios.post("/member/signUp", postSignUpPropsData)
   }
