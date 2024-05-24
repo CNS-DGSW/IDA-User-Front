189aa7f85e424bb7f9924019e30e2844343ef75b
chldkduds2 <101560578+chldkduds2@users.noreply.github.com>
2024-05-24 15:27:48 +0900

    feat : Implementing a FAQ skeletal UI

---
+++ src/components/FAQ/FAQitem/FAQskeleton/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const FAQskeletonMainContentContainer = styled.div`
+  display: block;
+  align-items: center;
+  margin-left: 18.3%;
+  width: 61%;
+  height: 100%;
+  padding-bottom: 1%;
+`
