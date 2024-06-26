fade532777fa96411404cbc13f3c03948194dcbe
greenfrog616 <greenfrog0616@gmail.com>
2024-06-26 20:57:53 +0900

    feat:: 리더쉽 체크 및 교내 수상 입력

--- src/components/Write/subject/awardTable/index.tsx
+++ src/components/Write/subject/awardTable/index.tsx
@@ -1,9 +1,56 @@
-import Checkbox from "@/components/common/Checkbox"
-import Input from "@/components/common/Input"
-import { Table } from "@/components/common/table"
-import React from "react"
+import Checkbox from "@/components/Common/Checkbox";
+import Input from "@/components/Common/Input";
+import { Table } from "@/components/Common/table";
+import React, { useState, ChangeEvent } from "react";
 
-const AwardTable = () => {
+interface SemesterThProps {
+  text: string;
+}
+
+interface GradeHeaderProps {
+  grade: string;
+}
+
+const SemesterTh: React.FC<SemesterThProps> = ({ text }) => (
+  <Table.Th
+    width="59"
+    background="F9F9F9"
+    color="000"
+    style={{ fontSize: "12px", fontStyle: "normal" }}
+    notBorder
+  >
+    {text}
+  </Table.Th>
+);
+
+const GradeHeader: React.FC<GradeHeaderProps> = ({ grade }) => (
+  <Table.Th width="118" colspan={2}>
+    <Table.Tr height="31">
+      <Table.Th width="118" colspan={2} notBorder>
+        {grade}
+      </Table.Th>
+    </Table.Tr>
+    <Table.Tr height="31">
+      <SemesterTh text="1학기" />
+      <SemesterTh text="2학기" />
+    </Table.Tr>
+  </Table.Th>
+);
+
+const AwardTable: React.FC = () => {
+  const [checkedStates, setCheckedStates] = useState<boolean[]>(Array(6).fill(false));
+  const [awardInput, setAwardInput] = useState<string>("");
+
+  const handleCheckboxChange = (index: number) => {
+    const updatedStates = [...checkedStates];
+    updatedStates[index] = !updatedStates[index];
+    setCheckedStates(updatedStates);
+  };
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setAwardInput(event.target.value);
+  };
+
   return (
     <Table>
       <Table.Header>
@@ -12,87 +59,9 @@ const AwardTable = () => {
           <Table.Th rowspan={2} width="186">
             설명
           </Table.Th>
-          <Table.Th width="118" colspan={2}>
-            <Table.Tr height="31">
-              <Table.Th width="118" colspan={2} notBorder>
-                1학년
-              </Table.Th>
-            </Table.Tr>
-            <Table.Tr height="31">
-              <Table.Th
-                width="59"
-                background="F9F9F9"
-                color="000"
-                style={{ fontSize: "12px", fontStyle: "normal" }}
-                notBorder
-              >
-                1학기
-              </Table.Th>
-              <Table.Th
-                width="59"
-                background="F9F9F9"
-                color="000"
-                style={{ fontSize: "12px", fontStyle: "normal" }}
-                notBorder
-              >
-                2학기
-              </Table.Th>
-            </Table.Tr>
-          </Table.Th>
-          <Table.Th width="118" colspan={2}>
-            <Table.Tr height="31">
-              <Table.Th width="118" colspan={2} notBorder>
-                2학년
-              </Table.Th>
-            </Table.Tr>
-            <Table.Tr height="31">
-              <Table.Th
-                width="59"
-                background="F9F9F9"
-                color="000"
-                style={{ fontSize: "12px", fontStyle: "normal" }}
-                notBorder
-              >
-                1학기
-              </Table.Th>
-              <Table.Th
-                width="59"
-                background="F9F9F9"
-                color="000"
-                style={{ fontSize: "12px", fontStyle: "normal" }}
-                notBorder
-              >
-                2학기
-              </Table.Th>
-            </Table.Tr>
-          </Table.Th>
-          <Table.Th width="118" colspan={2}>
-            <Table.Tr height="31">
-              <Table.Th width="118" colspan={2} notBorder>
-                3학년
-              </Table.Th>
-            </Table.Tr>
-            <Table.Tr height="31">
-              <Table.Th
-                width="59"
-                background="F9F9F9"
-                color="000"
-                style={{ fontSize: "12px", fontStyle: "normal" }}
-                notBorder
-              >
-                1학기
-              </Table.Th>
-              <Table.Th
-                width="59"
-                background="F9F9F9"
-                color="000"
-                style={{ fontSize: "12px", fontStyle: "normal" }}
-                notBorder
-              >
-                2학기
-              </Table.Th>
-            </Table.Tr>
-          </Table.Th>
+          <GradeHeader grade="1학년" />
+          <GradeHeader grade="2학년" />
+          <GradeHeader grade="3학년" />
         </Table.Tr>
       </Table.Header>
       <Table.Body>
@@ -104,26 +73,17 @@ const AwardTable = () => {
             style={{ fontSize: "12px", padding: "0 18px" }}
           >
             최소 한 학기 이상 학생회 임원 ( 전교 학생회장, 전교
-            학생부회장,학급반장 )
+            학생부회장, 학급반장 )
           </Table.Td>
-          <Table.Td width="59">
-            <Checkbox checked={false} name="leader" />
-          </Table.Td>
-          <Table.Td width="59">
-            <Checkbox checked={false} name="leader" />
-          </Table.Td>
-          <Table.Td width="59">
-            <Checkbox checked={false} name="leader" />
-          </Table.Td>
-          <Table.Td width="59">
-            <Checkbox checked={false} name="leader" />
-          </Table.Td>
-          <Table.Td width="59">
-            <Checkbox checked={false} name="leader" />
-          </Table.Td>
-          <Table.Td width="59">
-            <Checkbox checked={false} name="leader" />
-          </Table.Td>
+          {Array(6).fill(null).map((_, index) => (
+            <Table.Td key={index} width="59">
+              <Checkbox
+                checked={checkedStates[index]}
+                onChange={() => handleCheckboxChange(index)}
+                name={`leader-${index}`}
+              />
+            </Table.Td>
+          ))}
         </Table.Tr>
         <Table.Tr>
           <Table.Td background="F9F9F9">리더쉽</Table.Td>
@@ -137,13 +97,15 @@ const AwardTable = () => {
           <Table.Td width="354" colspan={6}>
             <Input
               type="text"
+              value={awardInput}
+              onChange={handleInputChange}
               style={{ margin: "0 auto", height: "36px", textAlign: "center" }}
             />
           </Table.Td>
         </Table.Tr>
       </Table.Body>
     </Table>
-  )
-}
+  );
+};
 
-export default AwardTable
+export default AwardTable;
