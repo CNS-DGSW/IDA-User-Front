9899c6716b2efe6ac23b78022e6a9ac0c687ca36
bae0203 <gmbae06@dgsw.hs.kr>
2024-07-08 22:27:17 +0900

    fix : Photo Query 이름 변경

--- src/hooks/Write/queries/useIdPhotoQuery.ts
+++ src/hooks/Write/queries/useIdPhotoQuery.ts
@@ -2,7 +2,7 @@ import { getPhoto, putPhoto } from "@/apis/Write/idPhoto"
 import { QUERY_KEYS } from "@/queries/queryKey"
 import { type UseMutationResult, useMutation, useQuery } from "react-query"
 
-export const useGetPersonalInfo = () =>
+export const useGetPhoto = () =>
   useQuery(QUERY_KEYS.Write.getPhoto, getPhoto, {
     staleTime: 1000 * 60 * 60,
     cacheTime: 1000 * 60 * 60,
@@ -10,5 +10,5 @@ export const useGetPersonalInfo = () =>
     refetchOnMount: false,
   })
 
-export const usePutPersonalInfo = (updateInfo: FormData): UseMutationResult =>
+export const useFixPhoto = (updateInfo: FormData): UseMutationResult =>
   useMutation({ mutationFn: async () => await putPhoto(updateInfo) })
