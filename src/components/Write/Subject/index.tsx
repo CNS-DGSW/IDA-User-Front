1d8b3eedff0cccdb7c1b9fc04d94cdf50217451d
bae0203 <gmbae06@dgsw.hs.kr>
2024-07-29 19:30:51 +0900

    feat : 점수 갱신 버튼 생성

--- src/components/Write/Subject/index.tsx
+++ src/components/Write/Subject/index.tsx
@@ -4,16 +4,27 @@ import React from "react"
 import useSubjectTable from "./Hooks/useSubjectTable"
 import AcademicRecord from "./AcademicRecord"
 import GEDRecord from "./GEDRecord"
+import ScoreUpdateButton from "./ScoreUpdateButton"
 
 const WriteSubject = () => {
   const context = useSubjectTable()
 
-  //확인을 위해 임시로 만들어 둔 변수 입니다. 나중에 수정할 때 삭제 해주세요.
   const isAcademicRecord = false
+
+  const ReloadAcademicRecord = () => {
+    console.log("내신 성적 새로고침")
+  }
+
+  const ReloadGEDRecord = () => {
+    console.log("고입검정 성적 새로고침")
+  }
 
   return (
     <Card>
       <TableContextComponent name="subject-table" context={context}>
+        <ScoreUpdateButton
+          onClick={isAcademicRecord ? ReloadAcademicRecord : ReloadGEDRecord}
+        />
         {isAcademicRecord ? <AcademicRecord /> : <GEDRecord />}
       </TableContextComponent>
     </Card>
