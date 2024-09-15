e4049da2adfa5e73419bd7177e56ea86c5682d1d
bae0203 <gmbae06@dgsw.hs.kr>
2024-09-15 17:00:59 +0900

    feat : table style 훅(radius)

---
+++ src/components/Write/Subject/Hooks/useTableStyle.ts
@@ -0,0 +1,18 @@
+const useTableStyle = () => {
+  const setTableRadius = (x: number | null, y: number | null) => {
+    switch (x) {
+      case 0:
+        if (y == 1) return "0 0 0 8px"
+        else return "8px 0 0 0"
+      case 1:
+        if (y == 1) return "0 0 8px 0"
+        else return "0 8px 0 0"
+      default:
+        return "8px"
+    }
+  }
+
+  return { setTableRadius }
+}
+
+export default useTableStyle
